3db428f89d8f0cf3d17ac2d7b0ddaebb821a8c78
valyamoro <moralek228@gmail.com>
2024-03-18 20:02:34 +0300

    base feat (Добавил подключение к БД)

--- Backend/cmd/web/main.go
+++ Backend/cmd/web/main.go
@@ -1,1 +1,25 @@
 package main
+
+import "database/sql"
+import _ "github.com/go-sql-driver/mysql"
+
+func main() {
+	dsn := `root:mysql@tcp(127.0.0.1:3306)/dictionary?parseTime=true`
+	_, err := openDB(dsn)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open(`mysql`, dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
