45951389bf227e85e7490aa25d982706937e5fc4
valyamoro <moralek228@gmail.com>
2024-03-18 22:41:34 +0300

    validate feat (Добавил правила валидации)

---
+++ Backend/internal/validator/validator.go
@@ -0,0 +1,35 @@
+package validator
+
+import (
+	"regexp"
+	"strings"
+	"unicode/utf8"
+)
+
+var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+func MinChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) >= n
+}
+
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
+}
+
+func NotBlank(value string) bool {
+	return strings.TrimSpace(value) != ``
+}
+
+func MaxChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) <= n
+}
+
+func PermittedInt(value int, permittedValues ...int) bool {
+	for i := range permittedValues {
+		if value == permittedValues[i] {
+			return true
+		}
+	}
+
+	return false
+}
